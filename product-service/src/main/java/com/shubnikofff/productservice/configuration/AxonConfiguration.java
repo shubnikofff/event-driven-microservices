b8d4fa0a783b5d520a9f82eec844b944d1d1f039
Alexey Shubnikov <alexey.shubnikov@monese.com>
2022-06-21 12:07:14 +0300

    Section 23: Added snapshots for events

--- product-service/src/main/java/com/shubnikofff/productservice/configuration/AxonConfiguration.java
+++ product-service/src/main/java/com/shubnikofff/productservice/configuration/AxonConfiguration.java
@@ -1,6 +1,9 @@
 package com.shubnikofff.productservice.configuration;
 
 import com.thoughtworks.xstream.XStream;
+import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.Snapshotter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +15,10 @@ public class AxonConfiguration {
 		final var xStream = new XStream();
 		xStream.allowTypesByWildcard(new String[]{"com.shubnikofff.**"});
 		return xStream;
+	}
+
+	@Bean(name = "productSnapshotTriggerDefinition")
+	public SnapshotTriggerDefinition productSnapshotTriggerDefinition(Snapshotter snapshotter) {
+		return new EventCountSnapshotTriggerDefinition(snapshotter, 3);
 	}
 }
